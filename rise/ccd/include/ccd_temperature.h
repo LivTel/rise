1d2f080ca456cffc529296b6e42bef97d5f201b9
Chris Mottram <C.Mottram@ljmu.ac.uk>
2022-03-14 15:58:54 +0000

    Removed CCD_Temperature_Get_Utility_Board_ADU / CCD_Temperature_Get_Heater_ADU (SDSU specific API).

--- rise/ccd/include/ccd_temperature.h
+++ rise/ccd/include/ccd_temperature.h
@@ -18,21 +18,22 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 /* ccd_temperature.h
-** $Header: /space/home/eng/cjm/cvs/rise/ccd/include/ccd_temperature.h,v 1.1 2009-10-15 10:16:27 cjm Exp $
+** $Header: /space/home/eng/cjm/cvs/rise/ccd/include/ccd_temperature.h,v 1.2 2022-03-14 15:58:51 cjm Exp $
 */
 #ifndef CCD_TEMPERATURE_H
 #define CCD_TEMPERATURE_H
 
 extern int CCD_Temperature_Get(double *temperature);
-extern int CCD_Temperature_Get_Utility_Board_ADU(int *adu);
 extern int CCD_Temperature_Set(double target_temperature);
-extern int CCD_Temperature_Get_Heater_ADU(int *heater_adu);
 extern int CCD_Temperature_Get_Error_Number(void);
 extern void CCD_Temperature_Error(void);
 extern void CCD_Temperature_Error_String(char *error_string);
 
 /*
 ** $Log: not supported by cvs2svn $
+** Revision 1.1  2009/10/15 10:16:27  cjm
+** Initial revision
+**
 ** Revision 0.4  2006/05/16 14:15:33  cjm
 ** gnuify: Added GNU General Public License.
 **
